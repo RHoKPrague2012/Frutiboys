133b13230a26ad0141e58efd2b5786d89e7b83ff
Vlastimil Dolejs <vlasta.dolejs@gmail.com>
2012-06-02 15:12:29 +0200

    REST API implementation

---
+++ app/controllers/OrganizationsRest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package controllers;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import models.Organization;
+
+import org.apache.commons.lang.StringUtils;
+
+import play.mvc.Controller;
+
+import com.google.common.collect.Lists;
+
+/**
+ * @author Vlastimil Dolejs (vlasta.dolejs@gmail.com)
+ *
+ */
+public class OrganizationsRest extends Controller {
+
+	public static void organizations() {
+		Map<String, String> parameters = params.allSimple();
+		parameters.remove("body"); // hack - play puts empty body parameter to each request???
+		
+		String queryFindBy = "";
+		List<String> queryParams = Lists.newArrayList();
+
+		for (Entry<String, String> entry : parameters.entrySet()) {
+			String fieldName = entry.getKey();
+			fieldName = StringUtils.lowerCase(fieldName);
+			fieldName = StringUtils.capitalize(fieldName);
+			
+			String value = entry.getValue();
+			value = value.replace("*", "%");
+			
+			if (StringUtils.isEmpty(queryFindBy)) {
+				queryFindBy = "by";
+			} else {
+				queryFindBy += "And";
+			}
+			
+			queryFindBy += fieldName + "Ilike";
+			queryParams.add(value);
+		}
+		
+		List<Organization> organizations;
+		if (StringUtils.isEmpty(queryFindBy)) {
+			organizations = Organization.all().fetch();
+		} else {
+			organizations = Organization.find(queryFindBy, queryParams.toArray()).fetch();
+		}
+		
+		if (organizations.isEmpty()) {
+			// nothing found
+		} else if (organizations.size() == 1) {
+			renderJSON(organizations.get(0));
+		} else {
+			renderJSON(organizations);
+		}
+	}
+}
